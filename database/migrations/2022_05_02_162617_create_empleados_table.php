c90e16900b1c410e4ee1c4383ece7f737bc0650f
Alex <alexander221b@hotmail.com>
2022-05-02 11:53:23 -0500

    migrate empleados table

--- database/migrations/2022_05_02_162617_create_empleados_table.php
+++ database/migrations/2022_05_02_162617_create_empleados_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('empleados', function (Blueprint $table) {
             $table->id();
+            $table->string('Nombre');
+            $table->string('ApellidoPaterno');
+            $table->string('ApellidoMaterno');
+            $table->string('Correo');
+            $table->string('Foto');
             $table->timestamps();
         });
     }
